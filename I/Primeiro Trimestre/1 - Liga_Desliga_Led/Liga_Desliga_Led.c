386b91eb402882a20e7ae533dbe7d19664bea8bb
RafaelVolkmer <85331378+RafaelVVolkmer@users.noreply.github.com>
2022-05-06 21:00:11 -0300

    Update Liga_Desliga_Led.c

--- I/Primeiro Trimestre/1 - Liga_Desliga_Led/Liga_Desliga_Led.c
+++ I/Primeiro Trimestre/1 - Liga_Desliga_Led/Liga_Desliga_Led.c
@@ -8,7 +8,7 @@ const uint8_t Mascara = 0b01;
 
 const uint8_t LigaOuDesliga[2]=
 {
-	0b00 //desliga
+	0b00, //desliga
 	0b01 //Liga
 };
 
@@ -16,16 +16,16 @@ int main(void)
 {
 
       // Habilita clock dos registradores B e C
-      RCC->AHBE1NR|=RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN;
-	    
+      RCC->AHB1ENR|=RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN;
+
       //Entrada PB0
-      GPIOB->MODER&=~PIO_MODER_MODER0;
-      
+      GPIOB->MODER&=~GPIO_MODER_MODER0;
+
       //Zerando pinos C
-      GPIOC->MODER&=~PIO_MODER_MODER0;
+      GPIOC->MODER&=~GPIO_MODER_MODER0;
       //Saída PC0
-      GPIOC->MODER|=PIO_MODER_MODER0_0;
-      
+      GPIOC->MODER|=GPIO_MODER_MODER0_0;
+
       //Zerando PULL-DOWNS
       GPIOB->PUPDR&=~GPIO_PUPDR_PUPDR0;
       //PB0 é PULL-DOWN
@@ -35,12 +35,11 @@ int main(void)
         while (1)
        {
           	contador=GPIOB->IDR & Mascara; //Mascara com PB0
-		
+
 		//Se quiser que o led ligue quando for apertado o botão.
 		GPIOC->ODR=LigaOuDesliga[contador];
-		
+
 		//Se quiser que o led desligue quando for apertado o botão, descomente a linha abaixo e comente a anterior.
 		//GPIOC->ODR=~LigaOuDesliga[contador];
-			
+
        }
-}
