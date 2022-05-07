2b7cf7c6b4f317c061937513910e51808dcbd72c
RafaelVolkmer <85331378+RafaelVVolkmer@users.noreply.github.com>
2022-05-06 22:51:34 -0300

    Update Transdutor_Cardeais.c

--- I/Primeiro Trimestre/5 - Transdutor_Cardeais/Transdutor_Cardeais.c
+++ I/Primeiro Trimestre/5 - Transdutor_Cardeais/Transdutor_Cardeais.c
@@ -6,32 +6,34 @@ uint8_t contador;
 
 const uint8_t Mascara = 0b11110000;
 
+//Struct que dá nome às variáveis de saída.
 struct SaidasDisplay
 {
-	const uint8_t Apaga = 0b00000000;
-	const uint8_t N = 0b00110111;
-	const uint8_t S = 0b01101101;
-	const uint8_t E = 0b00111000;
-	const uint8_t O = 0b00111111;
-}Display;
+	int Apaga;
+	int N;
+	int S;
+	int E;
+	int O;
+};
 
+//Struct que dá nome às variáveis de entrada.
 struct EntradaTransdutor
 {
-	const uint8_t Norte = 0b0000;       // 0 decimal
-	const uint8_t Nordeste = 0b0101;    // 5 decimal
-	const uint8_t Leste = 0b0110;       // 6 decimal
-	const uint8_t Sudeste = 0b1111;     // 15 decimal
-	const uint8_t Sul = 0b1100;         // 12 decimal
-	const uint8_t Sudoeste = 0b1001;    // 9 decimal
-	const uint8_t Oeste = 0b1010;       // 10 decimal
-	const uint8_t Noroeste = 0b0011;    // 3 decimal
-}PontosCardeais;
+	int Norte;
+	int Nordeste;
+	int Leste;
+	int Sudeste;
+	int Sul;
+	int Sudoeste;
+	int Oeste;
+	int Noroeste;
+};
 
 int main(void)
 {
 
 	// Habilita clock dos registradores B, C e A
-	RCC->AHBE1NR|=RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN;
+	RCC->AHB1ENR|=RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN;
 
 	//Zerando os pinos C
 	GPIOC->MODER&=~(GPIO_MODER_MODER0 | GPIO_MODER_MODER1 | GPIO_MODER_MODER2 | GPIO_MODER_MODER3 | GPIO_MODER_MODER4 | GPIO_MODER_MODER5 | GPIO_MODER_MODER6);
@@ -51,53 +53,77 @@ int main(void)
 	//Entradas - PC4, PC5, PC6, PC7 são PULL UP
 	GPIOA->PUPDR|=GPIO_PUPDR_PUPDR4_0 | GPIO_PUPDR_PUPDR5_0 | GPIO_PUPDR_PUPDR6_0 | GPIO_PUPDR_PUPDR7_0;
 
+	//Declarando variáveis de saída.
+	struct SaidasDisplay Display;
 
+	Display.Apaga = 0b00000000;
+	Display.N = 0b00110111;
+	Display.S = 0b01101101;
+	Display.E = 0b00111000;
+	Display.O = 0b00111111;
+
+	//Declarando variáveis de entrada.
+	struct EntradaTransdutor PontosCardeais;
+
+	PontosCardeais.Norte = 0b0000;
+	PontosCardeais.Nordeste = 0b0101;
+	PontosCardeais.Leste = 0b0110;
+	PontosCardeais.Sudeste = 0b1111;
+	PontosCardeais.Sul = 0b1100;
+	PontosCardeais.Sudoeste = 0b1001;
+	PontosCardeais.Oeste = 0b1010;
+	PontosCardeais.Noroeste = 0b0011;
 
   while (1)
   {
 
+
 	 contador=GPIOA->IDR & Mascara; //Mascara IDR com as entradas PA4, PA5, PA6, PA7
-	 contador=contador >>4;
+	 contador=contador >>4; //Faz com que PA4, PA5, PA6 e PA7 tornem-se PA0, PA1, PA2 e PA3 para parâmetros de contagem.
 
-			  switch(contador)
-			 {
-			  case PontosCardeais.Norte:
-				  GPIOB->ODR=Display.N | GPIOC->ODR=Display.Apaga;
-				  	  	  	  break;
+	 	 	if(contador==PontosCardeais.Norte){
+	 	 	 	GPIOB->ODR=Display.N;
+	 	 	 	GPIOC->ODR=Display.Apaga;
+	 	 	}
 
-			  case PontosCardeais.Sul:
-				  GPIOB->ODR=Display.S | GPIOC->ODR=Display.Apaga;
-				  	  	  	  break;
+	 	 	if(contador==PontosCardeais.Sul){
+	 	 	 	GPIOB->ODR=Display.S;
+	 	 		GPIOC->ODR=Display.Apaga;
+	 	 	}
 
-			  case PontosCardeais.Leste:
-				  GPIOB->ODR=Display.E | GPIOC->ODR=Display.Apaga;
-			  				  break;
+	 	 	if(contador==PontosCardeais.Leste){
+	 	 		GPIOB->ODR=Display.E;
+	 	 		GPIOC->ODR=Display.Apaga;
+	 	 	}
 
-			  case PontosCardeais.Oeste:
-				  GPIOB->ODR=Display.O | GPIOC->ODR=Display.Apaga;
-			  				  break;
+	 	 	if(contador==PontosCardeais.Oeste){
+	 	 		GPIOB->ODR=Display.O;
+	 	 		GPIOC->ODR=Display.Apaga;
+	 	 	}
 
-			  case PontosCardeais.Nordeste:
-				  GPIOB->ODR=Display.N | GPIOC->ODR=Display.E;
-			  				  break;
+	 	 	if(contador==PontosCardeais.Nordeste){
+	 	 		GPIOB->ODR=Display.N;
+	 	 		GPIOC->ODR=Display.E;
+	 	 	}
 
-			  case PontosCardeais.Sudeste:
-				  GPIOB->ODR=Display.S | GPIOC->ODR=Display.E;
-			  				  break;
+	 	 	if(contador==PontosCardeais.Sudeste){
+	 	 		GPIOB->ODR=Display.S;
+	 	 		GPIOC->ODR=Display.E;
+	 	 	}
 
-			  case PontosCardeais.Noroeste:
-				  GPIOB->ODR=Display.N | GPIOC->ODR=Display.O;
-			  				  break;
+	 	 	if(contador==PontosCardeais.Noroeste){
+	 	 		GPIOB->ODR=Display.N;
+	 	 		GPIOC->ODR=Display.O;
+	 	 	}
 
-			  case PontosCardeais.Sudoeste:
-				  GPIOB->ODR=Display.S | GPIOC->ODR=Display.O;
-			  				  break;
-
-			  default:
-				  GPIOB->ODR=0b0 | GPIOC->ODR=0b0
-
-			 }
-
+	 	 	if(contador==PontosCardeais.Sudoeste){
+	 	 	 	GPIOB->ODR=Display.S;
+	 	 	 	GPIOC->ODR=Display.O;
+	 	 	}
+	 	 		else{
+	 	 			GPIOB->ODR=0b00;
+	 	 			GPIOC->ODR=0b00;
+	 	 		}
   }
 
 }
