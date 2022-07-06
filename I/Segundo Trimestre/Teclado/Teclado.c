769e00ad779be9b5695d8e60a9398a8c887851a7
Roza918 <45982532+Roza918@users.noreply.github.com>
2022-07-06 03:06:51 -0300

    Correção da variável Global 'Nota'. Fixed #1
    Declarei a variável "Nota" como ponteiro e adaptei o código para isso. Além disso foi necessário trocar o tipo da variável para uint32_t e adicionar volatile nela, pois verifiquei na biblioteca do "STM32F4xx.h" que o tipo do "ARR" é volatile uint32_t e um ponteiro necessita ser do mesmo tipo da variável para onde ele está apontando para um bom funcionamento.

--- I/Segundo Trimestre/Teclado/Teclado.c
+++ I/Segundo Trimestre/Teclado/Teclado.c
@@ -8,7 +8,7 @@
 uint8_t tempo;
 uint8_t teclas;
 uint8_t teclas_sustenidas;
-uint8_t nota;
+volatile uint32_t *nota;
 
 enum {DO=1,RE,MI=4,FA=8,SOL=16,LA=32,SI=64};
 enum {DOs=1,REs,MIs=4,FAs=8,SOLs=16,LAs=32,SIs=64};
@@ -17,14 +17,15 @@ enum {DOs=1,REs,MIs=4,FAs=8,SOLs=16,LAs=32,SIs=64};
 void chaveamento()
 {
 	if (teclas)
-			 {
-				 if (TIM10->SR & TIM_SR_UIF)
-				 {
-					GPIOA->ODR^=GPIO_ODR_ODR_7;
-				 }
-			 }
-			 else
-					GPIOA->ODR&=~GPIO_ODR_ODR_7;
+	{
+		if (TIM10->SR & TIM_SR_UIF)
+		{
+			GPIOA->ODR^=GPIO_ODR_ODR_7;
+		}
+	}else
+	{
+		GPIOA->ODR&=~GPIO_ODR_ODR_7;
+	}
 }
 
 int main(void)
@@ -88,86 +89,87 @@ int main(void)
 	//Habilitando contagem do TIMER10
 	TIM10->CR1 = TIM_CR1_CEN|TIM_CR1_ARPE;
 
-  while (1)
-  {
-	  //Mascaramentos
-	  teclas = GPIOC->IDR & mascara;
-	  teclas_sustenidas = GPIOB->IDR & mascara;
-	  tempo = TIM10->SR & TIM_SR_UIF;
-	  
-	  nota = TIM10->ARR;
+	while (1)
+	{
+		//Mascaramentos
+		teclas = GPIOC->IDR & mascara;
+		teclas_sustenidas = GPIOB->IDR & mascara;
+		tempo = TIM10->SR & TIM_SR_UIF;
 
-	  chaveamento();
+		nota = &(TIM10->ARR);
 
-		 switch(teclas)
-		 {
-		  	  case DO:
-		  		nota=1; //*calcular frequências em ARR para chegar nas notas
-		  		    break;
 
-		  	 case RE:
-		  		nota=1;
+
+		chaveamento();
+
+		switch(teclas)
+		{
+			case DO:
+				*nota=1; //*calcular frequências em ARR para chegar nas notas
+			    	break;
+
+			case RE:
+				*nota=1;
 		  			break;
 
-		  	 case MI:
-		  		nota=1;
+			case MI:
+				*nota=1;
 		  			break;
 
-		  	 case FA:
-		  		nota=1;
+			case FA:
+				*nota=1;
 		  			break;
 
-		  	 case SOL:
-		  		nota=1;
+			case SOL:
+				*nota=1;
 		  			break;
 
-		  	 case LA:
-		  		nota=1;
+			case LA:
+				*nota=1;
 		  			break;
 
-		  	 case SI:
-		  		nota=1;
+			case SI:
+				*nota=1;
 		  			break;
 
-		  	  default:
-		  		  nota=0;
-		  		  	break;
+			default:
+				*nota=0;
+		  	  		break;
 		 }
 
 		 switch(teclas_sustenidas)
 		 {
-		 		  	 case DOs:
-		 		  		nota=1;
-		 		  		    break;
+		 	 case DOs:
+		   		*nota=1;
+		   		    break;
 
-		 		  	 case REs:
-		 		  		nota=1;
-		 		  			break;
+		   	 case REs:
+		   		*nota=1;
+		   			break;
 
-		 		  	 case MIs:
-		 		  		nota=1;
-		 		  			break;
+		   	 case MIs:
+		   		*nota=1;
+		   			break;
 
-		 		  	 case FAs:
-		 		  		nota=1;
-		 		  			break;
+		   	 case FAs:
+		   		*nota=1;
+		   			break;
 
-		 		  	 case SOLs:
-		 		  		nota=1;
-		 		  			break;
+		   	 case SOLs:
+		   		*nota=1;
+		   			break;
 
-		 		  	 case LAs:
-		 		  		nota=1;
-		 		  			break;
+		   	 case LAs:
+		   		*nota=1;
+		   			break;
 
-		 		  	 case SIs:
-		 		  		nota=1;
-		 		  			break;
+		   	 case SIs:
+		   		*nota=1;
+		   			break;
 
-		 		  	  default:
-		 		  		  nota=0;
-		 		  		  	break;
+		  	  default:
+		  		*nota=0;
+		  		  	break;
 		 }
-
-  }
+	}
 }
